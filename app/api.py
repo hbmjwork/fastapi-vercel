70cda53bc2c59e18c954c5ae6d180be38e013273
hbmjwork <157254913+hbmjwork@users.noreply.github.com>
2024-04-07 21:08:20 -0300

    Update api.py

--- app/api.py
+++ app/api.py
@@ -1,6 +1,15 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
+from pydantic import BaseModel
+
+class HelloWorldResponse(BaseModel):
+    message: str
+  
 app = FastAPI()
 
 @app.get("/", tags=["Root"])
 async def hello():
   return {"message": "Hello World!"}
+
+@app.get("/helloworld", response_model=HelloWorldResponse)
+async def helloworld():
+    return HelloWorldResponse(message="Hello World!")
